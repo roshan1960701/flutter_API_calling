5839e65d7f72766ed2a2251625f3b6739b49fbf5
ROSHAN <roshanw1998@gmail.com>
2021-02-09 19:19:22 +0530

    Get API called in two ways

---
+++ lib/Screens/fetchPost.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class fetchPost extends StatefulWidget {
+  fetchPost({Key key}) : super(key: key);
+
+  @override
+  _fetchPostState createState() => _fetchPostState();
+}
+
+class _fetchPostState extends State<fetchPost> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
